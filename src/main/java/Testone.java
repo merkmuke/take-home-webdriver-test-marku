eb39d5f76867fb194a1366e6903c666d83d154b7
Marku <merkebie@gmail.com>
2020-09-09 19:10:13 -0400

    init commit. login attempt.

---
+++ src/main/java/Testone.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Testone {
+
+    public static void main(String args[]) {
+        //System.setProperty(“webdriver.chrome.driver”, “chromedriver.exe”);
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://localhost:7080/login");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.className("radius")).submit();
+        driver.close();
+
+
+
+
+
+    }
+}
