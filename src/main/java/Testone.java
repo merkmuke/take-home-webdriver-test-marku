eac34f94aaa789ef93ba0d3cb44502a38dba9723
Marku <merkebie@gmail.com>
2020-09-10 14:47:56 -0400

    test2

--- src/main/java/Testone.java
+++ src/main/java/Testone.java
@@ -5,12 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Testone {
 
     public static void main(String args[]) {
-        //System.setProperty(“webdriver.chrome.driver”, “chromedriver.exe”);
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://localhost:7080/login");
@@ -37,26 +38,20 @@ public class Testone {
         driver.manage().window().maximize();
         driver.get("http://localhost:7080/checkboxes");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        WebElement IChecked=driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[1]"));
+        WebElement IChecked = driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[1]"));
         IChecked.click();
-       //Unchecked element
-        WebElement IUnchecked=driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[2]"));
+        //Unchecked element
+        WebElement IUnchecked = driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[2]"));
         IUnchecked.click();
         //Conditions
-        if(IUnchecked.isSelected())
-        {
+        if (IUnchecked.isSelected()) {
             System.out.println("Checkbox is Toggled On");
-        }
-        else
-        {
+        } else {
             System.out.println("Checkbox is Toggled Off");
         }
-        if(IChecked.isSelected())
-        {
+        if (IChecked.isSelected()) {
             System.out.println("Checkbox ix Toggled On");
-        }
-        else
-        {
+        } else {
             System.out.println("Checkbox is Toggled Off");
         }
         driver.close();
@@ -66,23 +61,62 @@ public class Testone {
         driver.manage().window().maximize();
         driver.get("http://localhost:7080/drag_and_drop ");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        WebElement From=driver.findElement(By.xpath("//*[@id=\"column-a\"]"));
+        WebElement From = driver.findElement(By.xpath("//*[@id=\"column-a\"]"));
 
         //Using Action class for drag and drop.
-        Actions act=new Actions(driver);
+        Actions act = new Actions(driver);
 
         //Drag and Drop by Pixel.
-        act.dragAndDropBy(From,135, 40).build().perform();
+        act.dragAndDropBy(From, 135, 40).build().perform();
         driver.close();
 
-        //Test5
+        //Test5 Dropdown
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://localhost:7080/dropdown");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         Select drpOption = new Select(driver.findElement(By.xpath("//*[@id=\"dropdown\"]")));
         drpOption.selectByVisibleText("Option 1");
-        //driver.close();
+        driver.close();
+
+        //Test6 Dynamic content
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://localhost:7080/dynamic_content");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+
+        //get the list of all the links and images
+
+        List<WebElement> linksList = driver.findElements(By.xpath("//*[@id=\"content\"]/div[3]/div[1]/img"));
+
+        //linksList.addAll(driver.findElements(By.xpath("//*[@id=\"content\"]/div[3]/div[1]/img"));
+
+        System.out.println("size of full links and images-->" + linksList.size());
+
+        List<WebElement> activeLinks = new ArrayList<WebElement>();
+
+        //iterate linksList : exclude all the links or images which don’t have an href attribute
+
+        for (int i = 0; i < linksList.size(); i++) {
+
+            System.out.println(linksList.get(i).getAttribute("large-2 columns"));
+
+            if (linksList.get(i).getAttribute("large-2 columns") != null &&
+                    (!linksList.get(i).getAttribute("large-2 columns").contains("large-10 columns"))) {
+
+                activeLinks.add(linksList.get(i));
+                driver.close();
+
+                //Test 7 Dynamic Controls
+                driver = new ChromeDriver();
+                driver.manage().window().maximize();
+                driver.get("http://localhost:7080/dynamic_controls");
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                driver.findElement(By.xpath(("//*[@id=\"checkbox\"]")));
+
+            }
+        }
     }
 }
 
